d40bb328d1695f30644cfa51a265e6e0d12a4df6
Nakagawa3961 <136971870+Nakagawa3961@users.noreply.github.com>
2023-11-04 22:48:39 +0900

    :w new_filename.txt

--- src/routers/router.go
+++ src/routers/router.go
@@ -3,10 +3,13 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ryuta06012/tweet_backend/src/repositories"
+	"github.com/ryuta06012/tweet_backend/src/tweet"
 )
 
 func InitRouter(router *gin.Engine) *gin.Engine {
 	mysqlHandler := repositories.NewMysqlHandler()
 	userRouter(router, mysqlHandler)
+
+	router.POST("/tweet/", tweet.PostTweet)
 	return router
 }
