7e314cab93e7562a0eb7597047d97ec26971c959
Nakagawa3961 <136971870+Nakagawa3961@users.noreply.github.com>
2023-12-06 16:26:20 +0900

    デバッグ削除

--- src/usecases/tweet_usecase.go
+++ src/usecases/tweet_usecase.go
@@ -1,8 +1,6 @@
 package usecases
 
 import (
-	"fmt"
-
 	"github.com/ryuta06012/tweet_backend/src/models"
 	"github.com/ryuta06012/tweet_backend/src/repositories"
 	"github.com/ryuta06012/tweet_backend/src/views"
@@ -20,7 +18,6 @@ func NewTweetInteractor(mysqlHandler *repositories.MysqlRepository) *TweetIntera
 
 func (t *TweetInteractor) CreateTweetsRecord(request views.TweetRequest) error {
 	tweet := models.NewTweet(request.UserID, request.Text)
-	fmt.Printf("tweet.UserID: %v\n", tweet.UserID)
 	err := t.TweetRepository.CreateTweetRecord(tweet)
 	if err != nil {
 		return err
